7b7c20ae5d78df52adb497936746c50b93e06a68
INDHU C <41136512+indhulingam@users.noreply.github.com>
2018-07-20 12:33:10 +0530

    Create begin7th.java

---
+++ begin7th.java
@@ -0,0 +1,14 @@
+package INDHU;
+import java.util.Scanner;
+public class hello {
+	public static void main(String[] args)
+	{
+		Scanner scan=new Scanner(System.in);
+	    int n=scan.nextInt();
+	    for(int i=0;i<n;i++)
+	    {
+	    	System.out.println("hello");
+	    }
+	}
+
+}

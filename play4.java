d9b0cfc0b6edd68b5ce1e2cb73d8b27d85da0d8b
INDHU C <41136512+indhulingam@users.noreply.github.com>
2018-07-23 11:40:49 +0530

    Create play4.java

---
+++ play4.java
@@ -0,0 +1,19 @@
+package INDHU;
+import java.util.Scanner;
+public class play44 {
+	public static void main(String[] args){
+		Scanner scan=new Scanner(System.in);
+		String s=scan.next();
+		String n=s.substring(s.length()-1);
+		if(!(n.equals(".")))
+		  {
+			   System.out.print(s+".");
+			        
+		  }
+		else
+		 {
+		 String m=s.replace(".","");
+            System.out.println(m);			
+		 }
+}
+}

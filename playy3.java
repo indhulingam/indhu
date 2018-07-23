5b2897a342393b75f32c0ac1dac8c4b4c9385d1f
INDHU C <41136512+indhulingam@users.noreply.github.com>
2018-07-23 09:42:18 +0530

    Create playy3.java

---
+++ playy3.java
@@ -0,0 +1,25 @@
+package INDHU;
+import java.util.Scanner;
+public class play33 {
+	public static void main(String[] args)
+	{
+	Scanner scan=new Scanner(System.in);
+	int n=scan.nextInt();
+	int sum=0;
+	int a;
+	if(n!=0)
+	{
+	while(n!=0)
+	{
+     a=n%10;
+    sum=(sum*10)+a;
+     n/=10;
+     }
+	System.out.println(sum);
+	}
+	else
+	{
+		System.out.println("Invalid Input");
+	}
+	}
+}
